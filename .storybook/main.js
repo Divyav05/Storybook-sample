28d3195e221a609a5a7068041abe2bd5da6ed5fc
Divyav05 <divyav5504@gmail.com>
2026-04-02 23:34:04 +0530

    fix: total disable of minification and simplified stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,26 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ["../src/*.stories.@(js|jsx|ts|tsx)"], // Simplified glob
-  addons: [
-    "@chromatic-com/storybook",
-    "@storybook/addon-essentials",
-  ],
+  stories: ["../src/TodoItem.stories.jsx"], // Point directly to the file
+  addons: ["@chromatic-com/storybook", "@storybook/addon-essentials"],
   framework: {
     name: "@storybook/react-vite",
-    options: {
-      builder: {
-        viteConfigPath: ".storybook/vite.config.js", // We will create this next
+    options: {},
+  },
+  async viteFinal(config) {
+    const { mergeConfig } = await import('vite');
+    return mergeConfig(config, {
+      build: {
+        minify: false, // STOPS the wH[e] renaming
+        terserOptions: { mangle: false }, // Double protection
+        sourcemap: false,
       },
-    },
+      // This helps with path resolution in some Windows environments
+      resolve: {
+        alias: {
+          '@': '/src',
+        },
+      },
+    });
   },
 };
 export default config;
--- src/TodoItem.stories.jsx
+++ src/TodoItem.stories.jsx
@@ -5,23 +5,23 @@ export default {
   component: TodoItem,
 };
 
-// Use the Template pattern - this is much harder for minifiers to break
-const Template = (args) => <TodoItem {...args} />;
-
-export const NotDone = Template.bind({});
-NotDone.args = {
-  task: 'Learn Storybook',
-  isCompleted: false,
+export const NotDone = {
+  args: {
+    task: 'Learn Storybook',
+    isCompleted: false,
+  },
 };
 
-export const Finished = Template.bind({});
-Finished.args = {
-  task: 'Install React',
-  isCompleted: true,
+export const Finished = {
+  args: {
+    task: 'Install React',
+    isCompleted: true,
+  },
 };
 
-export const DesktopView = Template.bind({});
-DesktopView.args = {
-  task: 'Clean the kitchen, wash the dishes, and take out the trash',
-  isCompleted: false,
+export const DesktopView = {
+  args: {
+    task: 'Clean the kitchen, wash the dishes, and take out the trash',
+    isCompleted: false,
+  },
 };
